30c500dd55a328f0635b158eebc43a583e59a929
Savva Surenkov <savva@surenkov.space>
2021-11-18 11:57:37 +1000

    Synchonize tmux and emacs themes with alacritty

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -129,7 +129,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-  '(default :background "#0e1115"))
+  '(default :background "default"))
 
 (custom-theme-set-faces! 'doom-nord
-  '(default :background "#0e1115"))
+  '(default :background "default"))
