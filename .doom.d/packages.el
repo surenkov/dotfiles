3617f7d57331752d9b85ba9e9cb5572b201ba39b
Savva Surenkov <savva@surenkov.space>
2023-04-19 21:25:45 +0400

    Update doom emacs custom packages

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -55,12 +55,12 @@
 (package! nvm)
 (package! hledger-mode)
 (package! bazel)
-(package! lsp-ui :disable t)
+; (package! lsp-ui :disable t)
 (package! protobuf-mode)
 (package! ts-fold)
 (package! polymode)
 
-(unpin! tree-sitter tree-sitter-langs evil-textobj-tree-sitter)
+(unpin! tree-sitter tree-sitter-langs evil-textobj-tree-sitter lsp-mode lsp-ui)
 
 (package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
 (package! with-editor :pin "391e76a256aeec6b9e4cbd733088f30c677d965b")
